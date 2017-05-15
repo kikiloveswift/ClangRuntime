9e27385ce7c8a36888a213f88e27fdd3b48e15b7
konglee <konglee873@gmail.com>
2017-05-15 13:29:07 +0800

    Initial Commit

---
+++ ClangRuntime/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ClangRuntime
+//
+//  Created by kong on 17/5/15.
+//  Copyright © 2017年 konglee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ClangRuntime/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ClangRuntime
+//
+//  Created by kong on 17/5/15.
+//  Copyright © 2017年 konglee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
